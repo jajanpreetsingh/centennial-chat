31b9c968d2c50501e6255558813c6c2d56fbb62c
LAPTOP-GPREV3N6\jajan <jajanpreetsingh15@gmail.com>
2018-11-05 16:46:44 -0500

    remove unused code

--- CentennialTalk/CentennialTalk.Main/ClientApp/src/app/home/home.component.ts
+++ CentennialTalk/CentennialTalk.Main/ClientApp/src/app/home/home.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FileService } from '../services/file.service';
-const speech = require('@google-cloud/speech');
 import { Router } from '@angular/router';
 
 declare var MediaRecorder: any;
-const fs = require('fs');
-
-const record = require('node-record-lpcm16');
-
-const client = new speech.SpeechClient();
 
 @Component({
   selector: 'app-home',
@@ -44,45 +38,6 @@ export class HomeComponent implements OnInit {
           this.fileService.saveFile(audioBlob).subscribe(res => { console.log(res); });
         });
       });
-  }
-
-  startTwo() {
-    const encoding = 'Encoding of the audio file, e.g. LINEAR16';
-    const sampleRateHertz = 16000;
-    const languageCode = 'BCP-47 language code, e.g. en-US';
-
-    const request = {
-      config: {
-        encoding: encoding,
-        sampleRateHertz: sampleRateHertz,
-        languageCode: languageCode,
-      },
-      interimResults: false, // If you want interim results, set this to true
-    };
-
-    // Create a recognize stream
-    const recognizeStream = client
-      .streamingRecognize(request)
-      .on('error', console.error)
-      .on('data', data =>
-        process.stdout.write(
-          data.results[0] && data.results[0].alternatives[0]
-            ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
-            : `\n\nReached transcription time limit, press Ctrl+C\n`
-        )
-      );
-
-    record
-      .start({
-        sampleRateHertz: sampleRateHertz,
-        threshold: 0,
-        // Other options, see https://www.npmjs.com/package/node-record-lpcm16#options
-        verbose: false,
-        recordProgram: 'rec', // Try also "arecord" or "sox"
-        silence: '10.0',
-      })
-      .on('error', console.error)
-      .pipe(recognizeStream);
   }
 
   handleBlob(event) {
