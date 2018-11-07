1200540e6b095111bde580c3c45030002046a743
VadimGarkusha <vadgig@gmail.com>
2018-11-07 16:44:38 -0500

    Updated animation for 50 and 75 percent. Styles changes to general login. Added text to icons

--- CentennialTalk/CentennialTalk.Main/ClientApp/src/app/discussion/discussion.component.ts
+++ CentennialTalk/CentennialTalk.Main/ClientApp/src/app/discussion/discussion.component.ts
@@ -7,27 +7,22 @@ import { trigger, state, style, transition, animate, keyframes } from '@angular/
   styleUrls: ['./discussion.component.scss'],
   animations: [
     trigger('fiftyAnimation', [
-      transition('small <=> large', animate('700ms ease-in', keyframes([
-        style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
-        style({ opacity: 1, transform: 'scale(1.2)', offset: 0.4 }),
-        style({ opacity: 1, transform: 'translateY(35px)', offset: 0.5 }),
-        style({ opacity: 1, transform: 'scale(1)', offset: 0.99 }),
-        style({ opacity: 1, transform: 'translateY(0)', offset: 1.0 })
-      ]))),
+      transition('small <=> large', animate('1500ms ease-in', keyframes([
+        style({ color: '#744DA8', offset: 0.05 }),
+        style({ color: '#1FB3E0', offset: 0.20 }),
+        style({ color: '#49C219', offset: 0.35 }),
+        style({ color: '#F4DC2A', offset: 0.5 }),
+        style({ color: '#EEB417', offset: 0.65 }),
+        style({ color: '#D65129', offset: 0.8 }),
+        style({ color: '#D6CDCA', offset: 0.95 })
+        ]))),
     ]),
     trigger('seventyFiveAnimation', [
       transition('fixed <=> shaked', animate('1000ms ease-in', keyframes([
-        style({ opacity: 0, transform: 'translateX(-50%)', offset: 0.0 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #FFF, #744DA8)', transform: 'translateY(11%)', offset: 0.1 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #744DA8, #1FB3E0)',transform: 'translateX(30%)', offset: 0.2 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #1FB3E0, #49C219)',transform: 'translateY(-29%)', offset: 0.3 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #49C219, #F4DC2A)',transform: 'translateX(-25%)', offset: 0.4 }),
-        style({ opacity: 0.6, background: 'linear-gradient(to right, #F4DC2A, #EEB417)',transform: 'translateY(44%)', offset: 0.5 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #EEB417, #D65129)',transform: 'scale(1.5)', offset: 0.7 }),
-        style({ opacity: 0.2, background: 'linear-gradient(to right, #D65129, #D6CDCA)',transform: 'scale(1.2)', offset: 0.8 }),
-        style({ opacity: 1, background: 'linear-gradient(to right, #D6CDCA, #FFF)', transform: 'translateX(0)', offset: 0.9 }),
-        style({ opacity: 1, transform: 'scale(1)', offset: 0.95 }),
-        style({ opacity: 1, transform: 'translateY(0)', offset: 1.0  })
+        style({transform: 'scale(1.2)', offset: 0.2 }),
+        style({transform: 'scale(1.4)', offset: 0.5 }),
+        style({transform: 'scale(1.6)', offset: 0.8 }),
+        style({transform: 'scale(1)', offset: 1.0  })
       ])))
     ]),
   ]
